82de96782df155126d664b116420d6f2a4dde87e
Erik <erickopcha@gmail.com>
2020-02-20 10:32:41 +0200

    feat: desktop v.

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,10 @@
 'use scrict';
 
-var playButton = document.querySelector('.main__btn');
-var playOnline = document.querySelector('.main__link-online');
-var playTrailer = document.querySelector('.nav__link--trailer');
-var modalBlock = document.querySelector('.modal');
-var closeModal = document.querySelector('.modal__close');
+var playButton = document.querySelector('.main__btn'),
+      playOnline = document.querySelector('.main__link-online'),
+      playTrailer = document.querySelector('.nav__link--trailer'),
+      modalBlock = document.querySelector('.modal'),
+      closeModal = document.querySelector('.modal__close');
 
 function showElem(elem) {
   elem.style.display = 'block';
