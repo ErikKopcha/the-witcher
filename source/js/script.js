29f22a39b277b091563d6a33e787e36f46222891
Erik <erickopcha@gmail.com>
2020-02-21 21:55:41 +0200

    feat: v1.4

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,9 @@ var playButton = document.querySelector('.main__btn'),
   playOnline = document.querySelector('.main__link-online'),
   playTrailer = document.querySelector('.nav__link--trailer'),
   modalBlock = document.querySelector('.modal'),
-  closeModal = document.querySelector('.modal__close');
+  closeModal = document.querySelector('.modal__close'),
+  navButton = document.querySelector('.nav__button'),
+  navList = document.querySelector('.nav__list');
 
 function showElem(elem) {
   elem.style.display = 'block';
@@ -28,6 +30,11 @@ playTrailer.addEventListener('click', function () {
 
 closeModal.addEventListener('click', function () {
   hideElem(modalBlock);
+});
+
+navButton.addEventListener('click', function () {
+  navButton.classList.toggle('nav__button-active');
+  navList.classList.toggle('nav__list-db');
 });
 
 window.addEventListener('keydown', function (evt) {
